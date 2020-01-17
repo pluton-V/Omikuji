0b0ae1b519950ca5edd10a85737e467615eb670b
pluton-V <erza329@gmail.com>
2020-01-17 16:05:38 +0900

    OmikujiBox クラスの作成 画像ファイル追加

---
+++ app/src/main/java/com/example/omikuji/OmikujiBox.kt
@@ -0,0 +1,11 @@
+package com.example.omikuji
+
+import java.util.*
+
+class OmikujiBox {
+    val number : Int // くじ番号 (0～19の乱数)
+    get() {
+        val rnd = Random
+        return rnd.nextInt(20)
+    }
+}
