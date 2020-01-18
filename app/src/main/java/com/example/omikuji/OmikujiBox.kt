a76a9e9c8bb1db524365d4ab9fc21e0f0ffa3bef
pluton-V <erza329@gmail.com>
2020-01-18 22:49:55 +0900

    /add/ 6.1_1 OmikujiBox クラスにプロパティを追加

--- app/src/main/java/com/example/omikuji/OmikujiBox.kt
+++ app/src/main/java/com/example/omikuji/OmikujiBox.kt
@@ -1,11 +1,19 @@
 package com.example.omikuji
 
+import android.widget.ImageView
 import java.util.*
 
 class OmikujiBox {
+    lateinit var omikujiView: ImageView
+    var finish = false // 箱から出たか？
+
     val number : Int // くじ番号 (0～19の乱数)
     get() {
         val rnd = Random()
         return rnd.nextInt(20)
+    }
+
+    fun shake() {
+
     }
 }
