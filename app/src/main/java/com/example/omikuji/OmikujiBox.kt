33b630fecc54db78c3f7105f6f9e17d6b076b7d5
pluton-V <erza329@gmail.com>
2020-01-18 22:55:41 +0900

    /upgrade/ 6.1_2 shake メソッド追加

--- app/src/main/java/com/example/omikuji/OmikujiBox.kt
+++ app/src/main/java/com/example/omikuji/OmikujiBox.kt
@@ -1,6 +1,11 @@
 package com.example.omikuji
 
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.RotateAnimation
+import android.view.animation.TranslateAnimation
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.omikuji.*
 import java.util.*
 
 class OmikujiBox {
@@ -14,6 +19,20 @@ class OmikujiBox {
     }
 
     fun shake() {
+        val translate = TranslateAnimation(0f, 0f, 0f, -200f)
+        translate.repeatMode = Animation.REVERSE
+        translate.repeatCount = 5
+        translate.duration = 100
 
+        val rotate = RotateAnimation(0f, -36f, omikujiView.width/2f, omikujiView.height/2f)
+        rotate.duration = 200
+
+        val set = AnimationSet(true)
+        set.addAnimation(rotate)
+        set.addAnimation(translate)
+
+        omikujiView.startAnimation(set)
+
+        finish = true
     }
 }
