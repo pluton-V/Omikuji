d549b29b4dc8cf582c6072077e276b8a7c7e7c95
pluton-V <erza329@gmail.com>
2020-01-21 19:15:10 +0900

    /add/ 7.1_2 メニューから呼び出されるメソッドを追加

--- app/src/main/java/com/example/omikuji/OmikujiActivity.kt
+++ app/src/main/java/com/example/omikuji/OmikujiActivity.kt
@@ -2,8 +2,10 @@ package com.example.omikuji
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.fortune.*
 import kotlinx.android.synthetic.main.omikuji.*
@@ -60,6 +62,12 @@ class OmikujiActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val toast = Toast.makeText(this, item?.title, Toast.LENGTH_LONG)
+        toast.show()
+
+        return super.onOptionsItemSelected(item)
+    }
 
     fun onButtonClick(v:View) {
 
